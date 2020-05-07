3da93d50eddfdcd00f54e245775a38bf30fc0053
poguepotamus <matthewpogue606@gmail.com>
2020-05-06 23:47:56 -0500

    For spaces

--- salutem/resources/Endpoint.py
+++ salutem/resources/Endpoint.py
@@ -10,22 +10,22 @@ from salutem.Database import DatabaseAbstractionLayer
 from salutem.settings import getAPISettings
 
 class _SalutemAPI(Resource):
-	def get(self):
-		return "Hello, World"
+    def get(self):
+        return "Hello, World"
 
-	def _createDatabase(self):
-		# Creating an instance of the database with out database settings in mind
-		self._database = DatabaseAbstractionLayer()
+    def _createDatabase(self):
+        # Creating an instance of the database with out database settings in mind
+        self._database = DatabaseAbstractionLayer()
 
-	def _parseArguments(self, argumentList):
-		parser = reqparse.RequestParser()
-		print(request.headers, flush=True)
-		for argument in argumentList:
-			parser.add_argument(argument)
-		return parser.parse_args()
+    def _parseArguments(self, argumentList):
+        parser = reqparse.RequestParser()
+        print(request.headers, flush=True)
+        for argument in argumentList:
+            parser.add_argument(argument)
+        return parser.parse_args()
 
-	def _setupEndpoint(self, argumentList=None):
-		# Creating a database in self._database
-		self._createDatabase()
-		# Returning arguments parsed from an argument list
-		return self._parseArguments(argumentList) if argumentList is not None else None
+    def _setupEndpoint(self, argumentList=None):
+        # Creating a database in self._database
+        self._createDatabase()
+        # Returning arguments parsed from an argument list
+        return self._parseArguments(argumentList) if argumentList is not None else None
